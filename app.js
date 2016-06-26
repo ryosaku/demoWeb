889f24cc3e162b337360349eaa3033f4afaec4af
ryosaku <nachata.nita@gmail.com>
2016-06-26 14:59:14 +0700

    git push

---
+++ app.js
@@ -0,0 +1,15 @@
+import { Component } from '@angular2/core';
+import { bootstrap } from '@angular2/platform/browser';
+
+@Component({
+    selector: 'app',
+    template:`
+        <button>OK</button>
+    `
+})
+
+class App{
+
+}
+
+bootstrap(App);
---
+++ profile.js
@@ -0,0 +1,15 @@
+import { Component } from '@angular2/core';
+import { bootstrap } from '@angular2/platform/browser';
+
+@Component({
+    selector: 'profile',
+    template:`
+        <button>OK</button>
+    `
+})
+
+class Profile{
+
+}
+
+bootstrap(Profile);
